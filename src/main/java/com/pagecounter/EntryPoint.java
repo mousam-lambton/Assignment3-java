7bf48c820f943f609f93a07195131944e42e4dd7
mousam-soliish <mousam@soliish.com>
2025-03-07 00:00:43 -0500

    Add entry point to start the springboot application

--- src/main/java/com/pagecounter/EntryPoint.java
+++ src/main/java/com/pagecounter/EntryPoint.java
@@ -1,13 +1,13 @@
-package org.example.assignment3;
+package com.pagecounter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment3Application {
+public class EntryPoint {
 
     public static void main(String[] args) {
-        SpringApplication.run(Assignment3Application.class, args);
+        SpringApplication.run(EntryPoint.class, args);
     }
 
 }
